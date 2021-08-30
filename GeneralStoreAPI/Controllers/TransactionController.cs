b6a2537ebce104aa5223ddfd8162098a4bc40e6c
Angel Ward <msangelward@gmail.com>
2021-08-29 23:14:55 -0400

    Added Transaction Verification

--- GeneralStoreAPI/Controllers/TransactionController.cs
+++ GeneralStoreAPI/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
 ﻿using GeneralStoreAPI.Models;
+using GeneralStorePI.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -24,12 +25,21 @@ namespace GeneralStoreAPI.Controllers
                 return BadRequest("Your request body cannot be empty.");
             }
 
+            var customer = await _context.Customer.FindAsync(model.CustomerId);
+            var product = await _context.Product.FindAsync(model.ProductSKU);
+            var itemcount = await _context.Transaction.FindAsync(model.ItemCount);
+
+            var validationResult = this.ValidateTransaction (model, product, customer);
+
+
             if (ModelState.IsValid)
             {
                 //Store the model in the database
                 _context.Transaction.Add(model);
+                product.NumberInInventory = product.NumberInInventory - model.ItemCount;
                 int changeCount = await _context.SaveChangesAsync();
-                return Ok("Your Transaction was created.");
+                return Ok("Your Transaction was created and Inventory has been adjusted.");
+               
             }
 
             // The model is not valid, go ahead and reject it
@@ -60,6 +70,24 @@ namespace GeneralStoreAPI.Controllers
 
             return NotFound();
 
+        }
+
+        private string ValidateTransaction(Transaction transaction, Product product, Customer customer)
+
+        {
+            if (customer == null)
+                Console.WriteLine("Invalid Transaction: Customer does not exist.");
+
+            if (product == null)
+                Console.WriteLine("Invalid Transaction: Product does not exist.");
+
+            if (!product.IsInStock)
+                Console.WriteLine("Invalid Transaction: Product is not in stock.");
+
+            if (product.NumberInInventory < transaction.ItemCount)
+                Console.WriteLine("Invalid Transaction: Not enough product in stock to complete the order.");
+
+            return string.Empty;
         }
 
     }
